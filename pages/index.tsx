1b51058b737cb1373b087eec80c69475bfff1450
Yuanlin Lin <im.yuanlinLin@gmail.com>
2022-07-25 00:40:23 +0800

    Add footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import { useState } from 'react';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Link from 'next/link';
 import type { NextPage } from 'next';
 
 function getOptions(start: Time, end: Time, duration: 30 | 60) {
@@ -30,7 +31,7 @@ const Home: NextPage = () => {
           content="A tool that help you pick time with friends."/>
       </Head>
 
-      <div className="w-full px-8 mb-24">
+      <div className="w-full md:max-w-xl px-8 mb-24 mx-auto">
 
         <div className="w-full flex justify-center py-8">
           <img src="/PICKTIME.svg" width={120} alt="PICKTIME"/>
@@ -54,7 +55,7 @@ const Home: NextPage = () => {
             {t('create_event_time_input_label')}
           </p>
           <div className="w-full flex gap-4">
-            <div className="flex-1 relative -top-[10px] text-right">
+            <div className="relative -top-[10px] text-right">
               {leftTimeOptions.map(
                 (opt) => <p key={opt.toString()} style={{ height: 36 }}>
                   {opt.start.toString()}
@@ -74,7 +75,7 @@ const Home: NextPage = () => {
                 onChange={setSelected}
                 options={rightTimeOptions}/>
             </div>
-            <div className="flex-1 relative -top-[10px]">
+            <div className="relative -top-[10px]">
               {rightTimeOptions.map(
                 (opt) => <p key={opt.toString()} style={{ height: 36 }}>
                   {opt.start.toString()}
@@ -83,6 +84,34 @@ const Home: NextPage = () => {
           </div>
         </div>
 
+      </div>
+
+      <div className="w-full bg-gray-100 pt-12 pb-24 flex justify-center">
+        <div className="mx-8 w-full md:w-96">
+          <img
+            src="/PICKTIME.svg"
+            width={120}
+            alt="PICKTIME"
+            className="mb-12"/>
+          <div className="w-full flex">
+            <div className="flex flex-col flex-grow">
+              <p className="opacity-30 mb-4">Languages</p>
+              <Link passHref href="/" locale="en-US">
+                <a className="my-1">English</a>
+              </Link>
+              <Link passHref href="/" locale="zh-TW">
+                <a className="my-1">繁體中文</a>
+              </Link>
+              <Link passHref href="/" locale="zh-CN">
+                <a className="my-1">简体中文</a>
+              </Link>
+            </div>
+            <div className="flex-grow">
+              <p className="opacity-30 mb-4">Other Links</p>
+              <a href="https://github.com/yuaanlin/pick-time.io">GitHub</a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
