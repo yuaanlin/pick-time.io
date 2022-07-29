1520b564634192c2ef4aee82befb660af591b6dd
Yuanlin Lin <im.yuanlinLin@gmail.com>
2022-07-29 17:04:19 +0800

    trying to fix SSG on Vercel which cannot found locale config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     locales: ['en-US', 'zh-TW', 'zh-CN'],
     defaultLocale: 'en-US',
+    localePath: path.resolve('./public/locales')
   },
 };
