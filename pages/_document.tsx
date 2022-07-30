ade037342c6e9a5114f59efedb427b79ae61778f
Yuanlin Lin <im.yuanlinLin@gmail.com>
2022-07-30 11:39:45 +0800

    add Google Analysis tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,19 @@ export default function Document() {
       <Head>
         <link
           href="https://fonts.googleapis.com/css?family=Noto+Sans+TC:400,500,700,900&subset=chinese-traditional"
-          rel="stylesheet"></link>
+          rel="stylesheet"/>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-P01F9W2K3H"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-P01F9W2K3H');`,
+          }}
+        />
       </Head>
       <body>
         <Main/>
