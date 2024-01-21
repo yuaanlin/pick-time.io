b47329a46b0d91d23398e18219e79e8e9809e655
Yuanlin Lin <im.yuanlinlin@gmail.com>
2024-01-21 21:29:06 +0800

    chore: Add deployed on Zeabur badge in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,30 +9,37 @@ function Footer() {
         src="/PICKTIME.svg"
         width={120}
         alt="PICKTIME"
-        className="mb-12"/>
-      <div className="w-full flex">
+        className="mb-12"
+      />
+      <div className="w-full flex mb-12">
         <div className="flex flex-col flex-grow">
           <p className="opacity-30 mb-4">Languages</p>
           <Link
             passHref
             href={router.pathname}
             as={router.asPath}
-            locale="en-US">
-            <a className="my-1">English</a>
+            locale="en-US"
+            className="my-1"
+          >
+            English
           </Link>
           <Link
             passHref
             href={router.pathname}
             as={router.asPath}
-            locale="zh-TW">
-            <a className="my-1">繁體中文</a>
+            locale="zh-TW"
+            className="my-1"
+          >
+            繁體中文
           </Link>
           <Link
             passHref
             href={router.pathname}
             as={router.asPath}
-            locale="zh-CN">
-            <a className="my-1">简体中文</a>
+            locale="zh-CN"
+            className="my-1"
+          >
+            简体中文
           </Link>
         </div>
         <div className="flex-grow">
@@ -40,8 +47,17 @@ function Footer() {
           <a href="https://github.com/yuaanlin/pick-time.io">GitHub</a>
         </div>
       </div>
+      <a
+        href="https://zeabur.com?referralCode=yuaanlin&utm_source=pick-time.io"
+      >
+        <img
+          src="https://zeabur.com/deployed-on-zeabur-dark.svg"
+          alt="Deployed on Zeabur"
+        />
+      </a>
     </div>
-  </div>;
+  </div>
+  ;
 }
 
 export default Footer;
